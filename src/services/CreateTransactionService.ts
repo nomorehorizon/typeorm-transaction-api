b2a88e5a7ccca98c4ba539f2060c3dfaf6306462
nomorehorizon <lmachado72@outlook.com>
2021-04-06 12:15:28 -0300

    Execute Method CreateTransactionService

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,36 @@
 // import AppError from '../errors/AppError';
 
+import { getCustomRepository } from 'typeorm';
+
+import TransactionRepository from '../repositories/TransactionsRepository';
+
 import Transaction from '../models/Transaction';
 
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+  category: string;
+}
+
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  public async execute({
+    title,
+    value,
+    type,
+    category,
+  }: Request): Promise<Transaction> {
+    const TransactionsRepository = getCustomRepository(TransactionRepository);
+
+    const transaction = TransactionsRepository.create({
+      title,
+      value,
+      type,
+    });
+
+    await TransactionsRepository.save(transaction);
+
+    return transaction;
   }
 }
 
